ca2b6911f5cd96127b9ac858c405a15ed9d22ceb
miiskii <ktq2490321@stu.o-hara.ac.jp>
2025-11-12 14:40:24 +0900

    新規登録作成 html, css, jsのコード作成 htmlとcssは連携済み jsは非連携

---
+++ JS/script.js
@@ -0,0 +1,50 @@
+document.getElementById("registerForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // 各要素
+    const nickname = document.getElementById("nickname");
+    const email = document.getElementById("email");
+    const passwpord = document.getElementById("password");
+
+    // エラーメッセージ要素
+    const nicknameError = document.getElementById("nickname-error");
+    const emailError = document.getElementById("email-error");
+    const passwordError = document.getElementById("password-error");
+
+    // エラー初期化
+    nicknameError.style.display = "none";
+    emailError.style.display = "none";
+    passwordError.style.display = "none";
+
+    let hasError = false;
+
+    // ニックネーム
+    if (nickname.value.trim() === "") {
+        nicknameError.textContent ="ニックネームを入力してください。";
+        nicknameError.style.display = "block";
+        hasError = true;
+    }
+
+    // メールアドレス
+    if (email.value.trim() === "") {
+        emailError.textContent = "メールアドレスを入力してください。";
+        emailError.style.display = "block";
+        hasError = true;
+    } 
+
+    // パスワード
+    if (passwpord.value.trim() === "") {
+        passwordError.textContent = "パスワードを入力してください。";
+        passwordError.style.display = "block";
+        hasError = true;
+    } else if (!halfWidthAlphaNum.test(passwordValue)) {
+        passwordError.textContent = "パスワードは半角英数字で入力してください。";
+        passwordError.style.display = "block";
+        hasError = true;
+    }
+
+    // エラーがなければフォーム送信
+    if (!hasError) {
+        this.submit();
+    }
+});
